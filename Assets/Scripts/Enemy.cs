030403c1390c340c555160730f8d6c2cd8c71055
SamVyK <143668835+SamVyK@users.noreply.github.com>
2023-10-05 14:20:04 +1100

    50%

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,6 +5,21 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     public int marks = 200;
+
+    public EnemyMovement enemyMovement { get; private set; }
+    public EnemyBase enemyBase { get; private set; }
+    public EnemyChase enemyChase { get; private set; }
+    public EnemyScatter enemyScatter { get; private set; }
+    public EnemyWeak enemyWeak { get; private set; }
+    public EnemyBehavior startingBehavior;
+    public Transform attack;
+
+    void Awake()
+    {
+        this.enemyMovement = GetComponent<EnemyMovement>();
+        this.enemyBase = GetComponent<EnemyBase>();
+        this.enemyChase = GetComponent<EnemyChase>();
+    }
     // Start is called before the first frame update
     void Start()
     {
---
+++ Assets/Scripts/EnemyBehavior.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Enemy))]
+public abstract class EnemyBehavior : MonoBehaviour
+{
+    public Enemy enemy { get ; private set; }
+    public float period;
+    private void Awake()
+    {
+        this.enemy = GetComponent<Enemy>();
+        this.enabled = false;
+    }
+
+    public void EnableEnemy()
+    {
+        EnableEnemy(this.period);
+    }
+
+    public virtual void EnableEnemy(float period)
+    {
+        this.enabled = true;
+        CancelInvoke();
+        Invoke(nameof(DisableEnemy), period);
+    }
+
+    public virtual void DisableEnemy()
+    {
+        this.enabled = false;
+        CancelInvoke();
+    }
+ 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
